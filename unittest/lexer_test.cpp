c13711403ffd5f8fec5a5e758338572e5c359953
Hubert Gruniaux <hubert@gruniaux.com>
2023-09-26 15:48:02 +0200

    Add unit testing for the lexer

---
+++ unittest/lexer_test.cpp
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+
+#include "lexer.hpp"
+
+class LexerTest : public ::testing::Test {
+public:
+  IdentifierTable identifier_table;
+
+  void SetUp() {
+    identifier_table.register_keywords();
+  }
+};
+
+TEST_F(LexerTest, punctuation) {
+  Lexer lexer(identifier_table, "= ,");
+  Token token;
+
+  lexer.tokenize(token);
+  EXPECT_EQ(token.kind, TokenKind::EQUAL);
+
+  lexer.tokenize(token);
+  EXPECT_EQ(token.kind, TokenKind::COMMA);
+
+  lexer.tokenize(token);
+  EXPECT_EQ(token.kind, TokenKind::EOI);
+}
+
+TEST_F(LexerTest, identifiers) {
+  Lexer lexer(identifier_table, "a c_out _l_2");
+  Token token;
+
+  lexer.tokenize(token);
+  EXPECT_EQ(token.kind, TokenKind::IDENTIFIER);
+
+  lexer.tokenize(token);
+  EXPECT_EQ(token.kind, TokenKind::IDENTIFIER);
+
+  lexer.tokenize(token);
+  EXPECT_EQ(token.kind, TokenKind::IDENTIFIER);
+
+  lexer.tokenize(token);
+  EXPECT_EQ(token.kind, TokenKind::EOI);
+}
+
+TEST_F(LexerTest, keywords) {
+  Lexer lexer(identifier_table, "OUTPUT VAR");
+  Token token;
+
+  lexer.tokenize(token);
+  EXPECT_EQ(token.kind, TokenKind::KEY_OUTPUT);
+
+  lexer.tokenize(token);
+  EXPECT_EQ(token.kind, TokenKind::KEY_VAR);
+
+  lexer.tokenize(token);
+  EXPECT_EQ(token.kind, TokenKind::EOI);
+}
