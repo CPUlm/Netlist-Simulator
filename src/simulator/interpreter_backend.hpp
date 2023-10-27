02132dd6f6c8e543a764b236db45420df9b3eb7a
Hubert Gruniaux <hubert@gruniaux.com>
2023-10-27 15:32:17 +0200

    Add `override` keyword to InterpreterBackend destructor.

--- src/simulator/interpreter_backend.hpp
+++ src/simulator/interpreter_backend.hpp
@@ -3,10 +3,24 @@
 
 #include "simulator.hpp"
 
+// ========================================================
+// class InterpreterBackend
+// ========================================================
+
+/// A very simple implementation of the SimulatorBackend API using a naive
+/// interpreter pattern.
+///
+/// This is intended as a base and reference implementation of the SimulatorBackend
+/// API. Moreover, it should be supported on all platforms. Also, the debugger API
+/// must be supported by this implementation. In other words, the interpreter is
+/// the simplest but the most complete implementation of the SimulatorBackend API.
+///
+/// The performance of this implementation are not a priority, still any
+/// improvement in time or memory consumption is welcome.
 class InterpreterBackend : public SimulatorBackend {
 public:
   InterpreterBackend();
-  ~InterpreterBackend();
+  ~InterpreterBackend() override;
 
   [[nodiscard]] std::string_view get_name() const override { return "interpreter"; }
 
