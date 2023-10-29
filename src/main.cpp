068e0cae9e9b6a8321b7dc935f48b978f26946f5
Hubert Gruniaux <hubert@gruniaux.com>
2023-10-29 14:53:51 +0100

    Add support for command line options and a real driver.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,131 @@
+#include "dependency_graph.hpp"
 #include "lexer.hpp"
 #include "parser.hpp"
 #include "simulator/simulator.hpp"
 
 #include <fstream>
+#include <iostream>
+
+struct CmdOptions {
+  std::string_view input_file;
+  std::string_view backend = "interpreter";
+  bool syntax_only = false;
+  bool dependency_graph = false;
+  bool schedule = false;
+  size_t cycles = 1;
+};
+
+static void print_help(const char *argv0) {
+  fmt::println("USAGE: {} [options...] input.net", argv0);
+  fmt::println("");
+  fmt::println("List of options:");
+  fmt::println("  -h, --help             Show this message.");
+  fmt::println("  -v, --version          Show the version of the program.");
+  fmt::println("  --cycles <n>           The count of cycles to simulate the program. The default is 1.");
+  fmt::println("  --backend <backend>    Selects a specific simulator backend.");
+  fmt::println("  --syntax-only          Only parses the input file, no scheduling or simulation is done.");
+  fmt::println("  --dep-graph            Outputs the dependency graph of the program in Graphviz DOT format.");
+  fmt::println("  --schedule             Outputs the scheduled program.");
+  fmt::println("");
+  fmt::println("List of backends:");
+  fmt::println("  interpreter            The classical interpreter backend, slow but the more complete.");
+}
+
+static void print_version() {
+  fmt::println("Netlist++ v1.0");
+}
+
+/// The command line parser.
+static CmdOptions parse_cmd_args(ReportManager &reports, int argc, const char *argv[]) {
+  // Ideally, it will be better to use an external library to do it.
+  // Like boost::program_options. But adding another dependency will
+  // complicate the build process. So we implement it ourselves, for the
+  // better and the worse.
+
+  CmdOptions options;
+
+  // The first element of argv is always, by the system conventions,
+  // the path of the program. So, we ignore it when parsing options.
+  bool parsing_options = true;
+  for (int i = 1; i < argc; ++i) {
+    const std::string_view arg = argv[i];
+
+    // All arguments after "--" should not be parsed as options.
+    if (parsing_options && arg == "--") {
+      parsing_options = false;
+      continue;
+    }
+
+    if (parsing_options) {
+      if (arg == "-h" || arg == "--help") {
+        print_help(argv[0]);
+        std::exit(EXIT_SUCCESS);
+      } else if (arg == "-v" || arg == "--version") {
+        print_version();
+        std::exit(EXIT_SUCCESS);
+      } else if (arg == "--cycles") {
+        if (i + 1 >= argc) {
+          reports.report(ReportSeverity::ERROR)
+              .with_message("command line option `--cycles' requires a parameter")
+              .finish()
+              .print();
+          print_help(argv[0]);
+          std::exit(EXIT_FAILURE);
+        }
+
+        options.cycles = std::stoull(argv[i++]);
+        continue;
+      } else if (arg == "--backend") {
+        if (i + 1 >= argc) {
+          reports.report(ReportSeverity::ERROR)
+              .with_message("command line option `--backend' requires a parameter")
+              .finish()
+              .print();
+          print_help(argv[0]);
+          std::exit(EXIT_FAILURE);
+        }
+
+        options.backend = argv[i++];
+        continue;
+      } else if (arg == "--syntax-only") {
+        options.syntax_only = true;
+        continue;
+      } else if (arg == "--schedule") {
+        options.schedule = true;
+        continue;
+      } else if (arg == "--dep-graph") {
+        options.dependency_graph = true;
+        continue;
+      }
+    }
+
+    if (!options.input_file.empty()) {
+      reports.report(ReportSeverity::ERROR)
+          .with_message("only a single Netlist input file is allowed")
+          .finish()
+          .print();
+      print_help(argv[0]);
+      std::exit(EXIT_FAILURE);
+    }
+
+    options.input_file = arg;
+  }
+
+  if (options.input_file.empty()) {
+    reports.report(ReportSeverity::ERROR).with_message("missing a Netlist input file").finish().print();
+    print_help(argv[0]);
+    std::exit(EXIT_FAILURE);
+  }
+
+  if (options.schedule && options.dependency_graph) {
+    reports.report(ReportSeverity::WARNING)
+        .with_message("command line options `--schedule' and `--dep-graph' are exclusives")
+        .finish()
+        .print();
+  }
+
+  return options;
+}
 
 [[nodiscard]] std::string read_file(std::string_view path) {
   // From https://stackoverflow.com/a/116220/8959979
@@ -25,23 +148,40 @@
 }
 
 int main(int argc, const char *argv[]) {
-  auto code = read_file(argv[1]);
-
   ReportManager report_manager;
-  report_manager.register_file_info(argv[1], code);
-  Lexer lexer(code.data());
+  const auto options = parse_cmd_args(report_manager, argc, argv);
+
+  std::string source_code;
+  try {
+    source_code = read_file(options.input_file);
+  } catch (const std::exception &) {
+    report_manager.report(ReportSeverity::ERROR)
+        .with_message("failed to read file `{}'", options.input_file)
+        .finish()
+        .exit();
+  }
+
+  report_manager.register_file_info(options.input_file, source_code);
+  Lexer lexer(source_code.data());
   Parser parser(report_manager, lexer);
   std::shared_ptr<Program> program = parser.parse_program();
 
-  if (program) {
-    Disassembler::disassemble(*program);
-
-    Simulator simulator(program);
-    simulator.set_register({ 0 }, 1);
-    simulator.set_register({ 1 }, 0);
-    simulator.print_inputs();
-    simulator.execute();
-    simulator.print_outputs();
+  if (!options.syntax_only && program != nullptr) {
+    DependencyGraph graph = DependencyGraph::build(program);
+    if (options.dependency_graph) {
+      graph.dump_dot();
+    } else if (options.schedule) {
+      graph.schedule();
+      Disassembler::disassemble(program);
+    } else {
+      graph.schedule();
+      Simulator simulator(program);
+      simulator.set_register({0}, 1);
+      simulator.set_register({1}, 0);
+      simulator.print_inputs();
+      simulator.execute();
+      simulator.print_outputs();
+    }
   }
 
   return EXIT_SUCCESS;
